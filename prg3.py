673a90a98c1dbc2bcff5303d910b19dfe40313d8
Pushilin-M <mvpooh@gmail.com>
2024-09-11 15:01:34 +0300

    Add files via upload

---
+++ prg3.py
@@ -0,0 +1,5 @@
+number_of_tasks = (12)
+number_of_hours = (1.5)
+nazvanie_kursa = ('Pyton')
+time_of_task = (number_of_hours / number_of_tasks)
+print('Курс:',nazvanie_kursa,', всего задач:',number_of_tasks,' , затрачено часов:',number_of_hours,' , среднее время выполнения',time_of_task,'часа.')
